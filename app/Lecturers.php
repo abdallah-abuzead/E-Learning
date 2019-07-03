8240f4bc1685401e65df8791d238a49166a1c53c
abdallah.abuzead.93@gmail.com <abdallah.abuzead.93@gmail.com>
2019-07-03 21:55:45 +0200

    add Courses and Lecturers Models, and CoursesController

---
+++ app/Lecturers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lecturers extends Model
+{
+    protected $table = 'lecturer';
+    protected $primaryKey = 'id';
+    protected $fillable = ['fullName', 'username', 'password', 'email', 'title'];
+    public function courses()
+    {
+        return $this->hasMany('App\Courses');
+    }
+}
