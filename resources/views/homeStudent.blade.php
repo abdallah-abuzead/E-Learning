278c6530dafa13bdbf835c55c7254072a14392c0
abdallah.abuzead.93@gmail.com <abdallah.abuzead.93@gmail.com>
2019-07-04 22:55:58 +0200

    some updates

--- resources/views/homeStudent.blade.php
+++ resources/views/homeStudent.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
 @section('content')
     <div class="container">
         <div class="row">
             @foreach ($courses as $course)
             <div class="col-sm-6 col-md-4">
-                <div class="thumbnail item-box">
-                    <img class="img-responsive" src={{asset("course.jpg")}}>
+                <div class="thumbnail">
+                    <img class="img-responsive " src={{asset("images/course.jpg")}}>
                     <div class="caption">
                         <h3><a href="/courses/{{$course->id}}"> {{$course->subject}}</a></h3>
-                        <p>Added by: <a href="/lecturer/{{$course->lec_id}}"> {{$course->lec_id}} </a></p>
+                        <p>Added by: {{$course->lec_id}}</p>
                     </div>
                 </div>
             </div>
