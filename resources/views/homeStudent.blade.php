53532f114dc315abde1f6d73d09f8895f3daf122
abdallah.abuzead.93@gmail.com <abdallah.abuzead.93@gmail.com>
2019-07-04 23:04:09 +0200

    another update

--- resources/views/homeStudent.blade.php
+++ resources/views/homeStudent.blade.php
@@ -7,7 +7,7 @@
             @foreach ($courses as $course)
             <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
-                    <img class="img-responsive " src={{asset("images/course.jpg")}}>
+                    <img class="img-responsive center-block" src={{asset("images/course.jpg")}}>
                     <div class="caption">
                         <h3><a href="/courses/{{$course->id}}"> {{$course->subject}}</a></h3>
                         <p>Added by: {{$course->lec_id}}</p>
