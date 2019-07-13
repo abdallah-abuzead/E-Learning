0b0bdb4f6beda514e3204af2b84251895edbcb30
abdallah.abuzead.93@gmail.com <abdallah.abuzead.93@gmail.com>
2019-07-13 19:57:31 +0200

    just touch

---
+++ database/migrations/2019_07_13_170646_course.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Course extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('course', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('course', function (Blueprint $table) {
+            $table->string('description');
+        });
+    }
+}
